b187192499f4aa335d267bba0ddbe2a35949ac8c
Marc Moerkeberg <mc.christiansen@outlook.com>
2024-12-02 22:58:11 +0100

    FileHelper class with method for getting calling directory

---
+++ AdventOfCode2024/FileHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace AdventOfCode2024
+{
+	public static class FileHelper
+	{
+		/// <summary>
+		/// Returns the calling file's parent directory path.
+		/// </summary>
+		/// <exception cref="DirectoryNotFoundException"></exception>
+		public static string GetCurrentDirectory([CallerFilePath] string callerFilePath = "")
+		{
+			string? callinfFileDirectory = Directory.GetParent(callerFilePath)?.FullName ?? string.Empty;
+			if (string.IsNullOrEmpty(callinfFileDirectory)) throw new DirectoryNotFoundException("Could not find calling files parent directory.");
+
+			return callinfFileDirectory;
+		}
+	}
+}
